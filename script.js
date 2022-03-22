496487e6571577a67f7b4b3cc15b9e78085949a1
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-03-22 18:15:53 -0300

    feito - requisito 09

--- script.js
+++ script.js
@@ -16,19 +16,25 @@ adicionarTarefa.addEventListener('click', () => {
 
 function removeSlected() {
   for (let index = 0; index < questList.childNodes.length; index += 1) {
-    if (questList.childNodes[index].className === 'task selected') {
-      console.log('removing');
+    if (questList.childNodes[index].classList.contains('selected')) {
       questList.childNodes[index].style.backgroundColor = '#ffffff';
-      questList.childNodes[index].className = 'task';
+      questList.childNodes[index].classList.remove('selected');
     }
   }
 }
 
+questList.addEventListener('dblclick', (event) => {
+  const doubleClickTarget = event.target;
+  if (doubleClickTarget.classList.contains('task')) {
+    doubleClickTarget.classList.toggle('completed');
+  }
+});
+
 questList.addEventListener('click', (event) => {
   const clickTarget = event.target;
-  if (clickTarget.className === 'task') {
+  if (clickTarget.classList.contains('task')) {
     removeSlected();
-    clickTarget.classList.add('selected');
+    clickTarget.classList.toggle('selected');
     clickTarget.style.backgroundColor = 'gray';
   }
 });
