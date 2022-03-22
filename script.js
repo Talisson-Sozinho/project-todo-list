6feeeb5c38390f7e045c0b424c754e63dda997e7
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-03-22 17:16:01 -0300

    feito - requisito 07

--- script.js
+++ script.js
@@ -5,10 +5,18 @@ const questList = document.getElementById('lista-tarefas');
 function adicionarNaLista(string) {
   const novaTarefa = document.createElement('li');
   novaTarefa.innerText = string;
+  novaTarefa.className = 'task';
   return novaTarefa;
 }
 
 adicionarTarefa.addEventListener('click', () => {
   questList.appendChild(adicionarNaLista(inputText.value));
   inputText.value = '';
+});
+
+questList.addEventListener('click', (event) => {
+  const clickTarget = event.target;
+  if (clickTarget.className === 'task') {
+    clickTarget.style.backgroundColor = 'gray';
+  }
 });
